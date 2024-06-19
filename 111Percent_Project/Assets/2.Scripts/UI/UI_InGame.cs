dd822f530552c3e1ad2c85fc9eb692f2d6ad9357
seiyounggee <34727166+seiyounggee@users.noreply.github.com>
2024-06-19 22:38:24 +0900

    2024.06.19 22:38

--- 111Percent_Project/Assets/2.Scripts/UI/UI_InGame.cs
+++ 111Percent_Project/Assets/2.Scripts/UI/UI_InGame.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using TMPro;
 
 public class UI_InGame : UIBase
 {
+    [SerializeField] TextMeshProUGUI scoreTxt;
+    [SerializeField] TextMeshProUGUI comboTxt;
 
+    [SerializeField] List<GameObject> healthObj = new List<GameObject>();
+
+    public override void Show()
+    {
+        base.Show();
+
+        Clear();
+    }
+
+    public void Clear()
+    {
+        scoreTxt.SafeSetText(string.Empty);
+        comboTxt.SafeSetText(string.Empty);
+    }
 }
